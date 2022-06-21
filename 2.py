10d8dfd88bda78df51c8c38760f018455df63597
ildar <ildar.khuzin75@mail.ru>
2022-06-21 19:31:49 +0300

    финальный вариант, все сходится))

--- 2.py
+++ 2.py
@@ -3,12 +3,10 @@
 # иметь размер 5x2.
 
 import numpy as np
-
-a = np.array([[1, 2, 3, 3, 1],[6, 8, 11, 10, 7]])
+a = np.array([[1,6],[2,8],[3,11],[3,10],[1,7]])
 print(a)
-
-mean_a = a.mean(axis=1)
+mean_a = a.mean(axis=0)
 print(mean_a)
 
-a_centered = np.array([(a[0] - mean_a[0]),(a[1] - mean_a[1])])
+a_centered = np.array([(a[:,0] - mean_a[0]),(a[:,1] - mean_a[1])]).transpose()
 print(a_centered)
--- 3.py
+++ 3.py
@@ -3,11 +3,15 @@
 
 import numpy as np
 
-N = 2 # число наблюдений
+N = 5 # число наблюдений
 
-a = np.array([[1, 2, 3, 3, 1],[6, 8, 11, 10, 7]])
-mean_a = a.mean(axis=1)
-a_centered = np.array([(a[0] - mean_a[0]),(a[1] - mean_a[1])])
+a = np.array([[1,6],[2,8],[3,11],[3,10],[1,7]])
+print(a)
+mean_a = a.mean(axis=0)
+print(mean_a)
 
-a_centered_sp = np.dot(a_centered[0], a_centered[1])
+a_centered = np.array([(a[:,0] - mean_a[0]),(a[:,1] - mean_a[1])]).transpose()
+print(a_centered)
+
+a_centered_sp = np.dot(a_centered[:,0], a_centered[:,1])
 print(f'ковариация двух признаков массива а - {a_centered_sp / (N - 1)}')
