2066e8f4439b3f522d1604e224f879bfc621bbd3
ildar <ildar.khuzin75@mail.ru>
2022-06-21 15:36:46 +0300

    вторая готова)

--- 2.py
+++ 2.py
@@ -6,7 +6,9 @@ import numpy as np
 
 a = np.array([[1, 2, 3, 3, 1],[6, 8, 11, 10, 7]])
 print(a)
+
 mean_a = a.mean(axis=1)
 print(mean_a)
-a_centered = np.array([[(a[0] - mean_a[0])],[(a[1] - mean_a[1])]])
+
+a_centered = np.array([(a[0] - mean_a[0]),(a[1] - mean_a[1])])
 print(a_centered)
